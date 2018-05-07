3046e0494a6e9629ad9cfb81015bb8b796cd2eda
John Mandalakas <jnmandal+if-you-know-me-use-my-real-email@cobarde.soy>
2018-05-07 14:56:51 -0500

    Remove yahoo call, add new metrics, add finviz charts

--- bot.js
+++ bot.js
@@ -10,8 +10,10 @@ const TelegramUpdateQueue = require('./telegram/queue');
 let queue = new TelegramUpdateQueue();
 queue.onEnqueue = (update) => {
   let updateAmount = queue.updates.length;
-  console.log(`[${new Date()}] - Enqueued update_id: ${update.update_id}`);
-  console.log(`[${new Date()}] - Updates In queue: ${updateAmount}`);
+  if (process.env.DEBUG) {
+    console.log(`[${new Date()}] - Enqueued update_id: ${update.update_id}`);
+    console.log(`[${new Date()}] - Updates In queue: ${updateAmount}`);
+  }
 }
 
 queue.listen();
@@ -22,51 +24,69 @@ setInterval(() => {
   if (queue.updates.length > 0) {
     let update = queue.dequeue();
     let updateAmount = queue.updates.length;
-    console.log(`[${new Date()}] - Dequeued update_id: ${update.update_id}`);
-    console.log(`[${new Date()}] - Updates In queue: ${updateAmount}`);
-    process(update);
+    if (process.env.DEBUG) {
+      console.log(`[${new Date()}] - Dequeued update_id: ${update.update_id}`);
+      console.log(`[${new Date()}] - Updates In queue: ${updateAmount}`);
+    }
+    processUpdate(update);
   }
 }, 1);
 console.log(`[${new Date()}] - Message queue listener boot success`);
 
 const answerInlineQuery = require('./telegram/inline').answerInlineQuery;
 const getQuote = require('./xignite/quotes').getQuote;
-const getSummary = require('./yahoo/api').getSummary;
+const getFundamentals = require('./xignite/fundamentals').getFundamentals;
 
 function process(update) {
   if (update.inline_query) {
-    console.log(`[${new Date()}] - query[telegram.inline],id[${update.inline_query.from.id}],handle[${update.inline_query.from.username}]`);
-    let data = { ticker: update.inline_query.query}
+    console.log(`[${new Date()}] - query[telegram.inline],id[${update.inline_query.from.id}],handle[${update.inline_query.from.username}],query[${update.inline_query.query}]`);
+    let data = { ticker: update.inline_query.query }
     Promise.all([
       getQuote(data.ticker)
         .then(response => {
           if (response.Outcome === 'Success') {
             // grab the pertinent data
-            data.name = response.Name;
-            data.price = response.Last;
-            data.priceTime = response.DateTime;
-            data.dayChange = response.PercentChange;
-          } else {
-            data.ticker = 'N/A';
+            data.name      = response['Name'];
+            data.price     = response['Last'];
+            data.dayChange = response['PercentChange'];
           }
         })
-        .catch(() => data.ticker = 'N/A'),
-      getSummary(data.ticker)
-        .then(response => {
-          const resData = response.quoteSummary.result[0];
-          data.yearChange = resData.defaultKeyStatistics["52WeekChange"].fmt;
+        .catch((err) => {
+          console.log('error in xignite superquote');
+          console.log(err);
+        }),
+      getFundamentals(data.ticker)
+        .then(fundamentals => {
+          data.beta           = fundamentals['Beta']
+          data.pe             = fundamentals['PERatio']
+          data.marketCap      = fundamentals['MarketCapitalization']
+          data.fourWeekHigh   = fundamentals['HighPriceLast4Weeks']
+          data.fourWeekLow    = fundamentals['LowPriceLast4Weeks']
+          data.weekChange     = fundamentals['PercentPriceChange1Week']
+          data.fourWeekChange = fundamentals['PercentPriceChange4Weeks']
+          data.yearChange     = fundamentals['PercentPriceChange52Weeks']
         })
-        .catch(() => data.yearChange = 'N/A')
+        .catch((err) => {
+          console.log('error in xignite factset fundamentals');
+          console.log(err);
+        })
     ]).then(() => {
-      let option = {
-        id: update.inline_query.query,
+      const summaryOption = {
+        id: `${update.inline_query.query}-${update.update_id}`,
         type: 'article',
-        title: `💰 ${data.ticker.toUpperCase()} - last: ${data.price}`,
+        title: `💰 ${data.ticker.toUpperCase()} - ${data.price} (${data.dayChange}%)`,
         input_message_content: {
-          message_text: `${data.name} (${data.ticker}) \nprice: ${data.price} \n@time: ${data.priceTime} \n% today change: ${data.dayChange} \n% 52 week change ${data.yearChange}`
+          message_text: `${data.name} (${data.ticker.toUpperCase()}) \nprice: ${data.price}\n% today change: ${data.dayChange}%\n% 7 day change: ${data.weekChange}%\n% 52 week change: ${data.yearChange}\nfour week high/low: ${data.fourWeekHigh}/${data.fourWeekLow}\nbeta: ${data.beta}\nPE ratio: ${data.pe}`
         }
       }
-      answerInlineQuery(update.inline_query.id, [option]);
+      const chartOption = {
+        id: `${update.inline_query.query}-${update.update_id}-photo`,
+        type: 'photo',
+        title: `📈 ${data.ticker.toUpperCase()} - Chart (52 weeks)`,
+        thumb_url: `https://finviz.com/chart.ashx?t=${data.ticker.toUpperCase()}&ty=l&s=m`,
+        photo_url: `https://finviz.com/chart.ashx?t=${data.ticker.toUpperCase()}&ty=l&ta=1`
+      }
+      answerInlineQuery(update.inline_query.id, data.price ? [summaryOption, chartOption] : []);
     });
   }
 }
---
+++ xignite/fundamentals.js
@@ -0,0 +1,44 @@
+const fetch = require('node-fetch');
+
+/*
+ * @function getFundamentals
+ * GETs comppany fundamentals from xignite
+ * @param quote - the ticker
+ */
+
+const fundamentalTypes = [
+  'MarketCapitalization',
+  'PERatio',
+  'HighPriceLast4Weeks',
+  'LowPriceLast4Weeks',
+  'PercentPriceChange52Weeks',
+  'PercentPriceChange1Week',
+  'Beta',
+  'PercentPriceChange4Weeks'
+]
+
+const asOfDate = () => {
+  let d = new Date();
+  return `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
+}
+
+function getFundamentals(ticker) {
+  const endpoint = `https://factsetfundamentals.xignite.com/xFactSetFundamentals.json/GetFundamentals` +
+    `?IdentifierType=Symbol&Identifiers=${ticker}&FundamentalTypes=${fundamentalTypes.join(',')}` +
+    `&AsOfDate=${asOfDate()}&ReportType=TTM&ExcludeRestated=False&UpdatedSince=` +
+    `&header_username=${process.env.XIGNITE_EMAIL}`
+  return fetch(endpoint)
+    .then(response => response.json())
+    .then(json => {
+      const data = {}
+      if (json[0]['Outcome'] == 'Success') {
+        const fList = json[0]['FundamentalsSets'][0]['Fundamentals'];
+        fList.forEach(fundamental => data[fundamental.Type] = fundamental.Value);
+      }
+      return data;
+    });
+}
+
+module.exports = {
+  getFundamentals
+}
